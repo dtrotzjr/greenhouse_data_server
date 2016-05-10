937395c43e8406c52360d70cce177722742d6254
David Trotz <david.trotz@gmail.com>
2016-05-09 22:29:21 -0700

    Reading settings from config.json

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-
-
+#include <iostream>
+#include <fstream>
+#include <json/json.h>
 #include "APWeatherData.h"
 
 #define CURRENT_WEATHER_URL "http://api.openweathermap.org/data/2.5/weather"
@@ -7,6 +8,16 @@
 
 int main(int argc, char* argv[]) 
 {
-    APWeatherData* wd = new APWeatherData();
-    wd->init("test.sqlite");
+    if (argc == 2) {
+        Json::Value root;
+        ifstream jsonFileStream;
+        infile.open(argv[1]);
+        jsonFileStream >> root;
+
+        APWeatherData* wd = new APWeatherData();
+        wd->init(root["sqlite3_file"]);
+        return 0;
+    }
+    std::cout << "Usage " + std::string(argv[1]) + " <path to config.json>" << std::endl;
+    return -1;
 }
