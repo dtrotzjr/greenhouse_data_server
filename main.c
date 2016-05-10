0b65481fd5543ca8702c7805d4dda5c56451d137
David Trotz <david.trotz@gmail.com>
2016-05-09 19:25:08 -0700

    Adding CLion project file

--- main.c
+++
@@ -1,9 +0,0 @@
-
-
-#define CURRENT_WEATHER_URL "http://api.openweathermap.org/data/2.5/weather"
-#define FIVE_DAY_FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast"
-
-int main(int argc, char* argv[]) 
-{
-	
-}
