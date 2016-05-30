7c9e588e26209f2cdb1c1a49f34dac6143bafba6
David Trotz <david.trotz@gmail.com>
2016-05-30 16:24:24 -0700

    Starting work on image transfer

---
+++ src/APImageTransferAgent.cpp
@@ -0,0 +1,13 @@
+//
+// Created by David Trotz on 5/30/16.
+//
+
+#include "APImageTransferAgent.h"
+
+APImageTransferAgent::APImageTransferAgent() {
+
+}
+
+APImageTransferAgent::~APImageTransferAgent() {
+
+}
---
+++ src/APImageTransferAgent.h
@@ -0,0 +1,17 @@
+//
+// Created by David Trotz on 5/30/16.
+//
+
+#ifndef GREENHOUSE_DATA_SERVER_APIMAGETRANSFERAGENT_H
+#define GREENHOUSE_DATA_SERVER_APIMAGETRANSFERAGENT_H
+
+
+class APImageTransferAgent {
+public:
+    APImageTransferAgent();
+    ~APImageTransferAgent();
+
+};
+
+
+#endif //GREENHOUSE_DATA_SERVER_APIMAGETRANSFERAGENT_H
