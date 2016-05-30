91849743e8140faabe27b5fe333ae3fc419e1b7c
David Trotz <david.trotz@gmail.com>
2016-05-29 23:47:06 -0700

    Fine tuning

--- src/APGreenhouse.cpp
+++ src/APGreenhouse.cpp
@@ -51,7 +51,7 @@ void APGreenhouse::GetLatestSensorData() {
     int maxTimestamp = _getMaxTimestampDataPoint();
     while (maxTimestamp > afterTimestamp) {
         afterTimestamp = maxTimestamp;
-        char *response = _getUpdateFeed(afterTimestamp, 100);
+        char *response = _getUpdateFeed(afterTimestamp, 25);
         if (response != NULL && strlen(response) > 0) {
             _parseJSONResponse(response);
         }
